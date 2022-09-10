ad1f348ca9a81cd88aa3b038753f6da053d36ec5
Rakesh <rakeshkota19@gmail.com>
2022-09-10 23:21:16 +1000

    Init Repo

---
+++ src/main/java/com/vikkyraki/prac_spring_batch/PracSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.vikkyraki.prac_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracSpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vikkyraki/prac_spring_batch/PracSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikkyraki.prac_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
